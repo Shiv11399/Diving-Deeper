846cecdb5b450c61f77fbed6095b44c78d4b5371
ShivDirtCube <shiv.bajpai.5@gmail.com>
2023-08-23 02:39:59 +0530

    working on player moving mechanism.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
+
+    /// <summary>
+    /// This method will move the player to respective target position.
+    /// </summary>
+    /// <param name="position"></param>
+   void MoveTo(Vector2 position)
+    {
+        transform.Translate(position); // THIS WILL WORK IF WE ARE MOVING IN A STRAIGHT LINE 
+    }
 }
