46c1378b1a7fabb0255290d5d91094a371f14274
ShivDirtCube <shiv.bajpai.5@gmail.com>
2023-08-23 02:36:08 +0530

    Creating A new unity Project + Working On grappling Mechanics

---
+++ Assets/Scripts/GrapplingGun.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingGun : MonoBehaviour
+{
+    [Range(0,1)]
+    [SerializeField] float RotationSpeedModifier = 0.5f;
+    [SerializeField] float MaxRotationAngle = 60f;
+
+    float tempOffset = 180;
+    void Update()
+    {
+
+        Vector3 eulerAngles = transform.eulerAngles;
+
+        float axis = (Input.GetAxis("Horizontal"));
+
+        float zOffset = (eulerAngles.z + axis);
+
+        zOffset = (zOffset > 180) ? zOffset - 360 : zOffset;
+
+        zOffset = Mathf.Clamp(zOffset, -MaxRotationAngle, MaxRotationAngle);
+
+        transform.eulerAngles = new Vector3(eulerAngles.x, eulerAngles.y, zOffset);
+    }
+
+
+}
